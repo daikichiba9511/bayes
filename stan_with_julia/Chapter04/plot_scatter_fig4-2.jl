7179ef551715948c70cc5565ff135b5431b941ad
daikichiba9511 <daikichiba.hokkaido@gmail.com>
2020-08-04 15:07:33 +0900

    add lm and fig4.3

--- stan_with_julia/Chapter04/plot_scatter_fig4-2.jl
+++ stan_with_julia/Chapter04/plot_scatter_fig4-2.jl
@@ -4,16 +4,19 @@
 
 using PyPlot
 using DataFrames
+using CSV
 
 function read_data()
-    CSV.read("./input/data-salary.txt")
+    CSV.read("./input/data-salary.txt", DataFrame)
 end
 
 function plot_scatter_fig4_2()
     df = read_data()
+    # confirm dataframes by show macro
     @show df
     @show df[!, 1];
     @show df[!, 2];
+    # create fig object of pyplot
     fig = figure()
     scatter(df[!, 1], df[!, 2])
     title("fig4.2")

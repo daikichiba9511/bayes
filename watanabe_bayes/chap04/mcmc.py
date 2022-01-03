33bf7d3948864604ed571f75bbc15c4c944e12aa
daikichiba9511 <daikichiba.hokkaido@gmail.com>
2022-01-03 17:56:07 +0900

    fix: metropolis

--- watanabe_bayes/chap04/mcmc.py
+++ watanabe_bayes/chap04/mcmc.py
@@ -1,56 +1,71 @@
+"""
+実装しかけ。メトロポリス法の詳細忘れた。元の自分のコードも間違ってる気がする
+"""
+from typing import Callable, Tuple, Union
+
 import numpy as np
 
-#TODO : 今は1変数でしかできないので多変数でもできるように拡張する
+
 class MetroPolis:
+    def __init__(
+        self,
+        loglikelihood_func: Callable[[np.ndarray], np.ndarray],
+        shape: Union[int, Tuple[int, int]] = 1,
+        dtype=np.float64,
+    ) -> None:
+        self._dtype = dtype
+        self._shape = shape
+        self._loglikelihood_func = loglikelihood_func
 
-    def __init__(self, loglikli):
-        self.loglikli = loglikli
+    def sample(
+        self, n_iter: int = 100, seed: int = 42, step_size: float = 0.5
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
-    def sample(self, n_iter=100, seed=42):
-        step_size = 0.5e0
-
+        # set global random seed
         np.random.seed(seed)
 
         # initialize
-        x = 0e0
-        n_accept_cnt = 0e0
+        x = np.zeros(shape=self._shape, dtype=self._dtype)
+        n_accept_cnt = 0
 
-        for iter in range(1, n_iter+1):
+        for _ in range(n_iter):
             backup_x = x
 
             # logliklihood in Statistics, but also Hamiltonian in Physics
-            action_init = self.loglikli(x)
-    
+            action_init = self._loglikelihood_func(x)
+
             # uniform random
-            dx = np.random.random()
-            dx = (dx - 0.5e0) * step_size * 2e0
+            dx = np.random.random(size=x.shape)
+            dx = (dx - 0.5) * step_size * 2
             x = x + dx
 
             # final state
-            action_fin = self.loglikli(x)
+            action_fin = self._loglikelihood_func(x)
 
             # testing reject or accept from here
-            metropolis = np.random.random()
-
+            # range : 0 ~ 1
+            metropolis = np.random.random(size=x.shape)
             # accept
-            if np.exp(action_init - action_fin) > metropolis:
+            # I mitht get wrong. so, should review this process after
+            if all(np.exp(action_init - action_fin) > metropolis):
                 n_accept_cnt += 1
 
             # reject
             else:
                 x = backup_x
 
-        return x, n_accept_cnt, n_accept_cnt/n_iter
+        return x, n_accept_cnt, n_accept_cnt / n_iter
 
-def metropolis_test():
 
-    def loglikli(x):
-        return 0.5 * x * x
+def test_metropolis():
+    def loglikelihood(x: np.ndarray) -> np.ndarray:
+        return 0.5 * x ** 2
 
-    metropolis = MetroPolis(loglikli=loglikli)
+    metropolis = MetroPolis(loglikelihood)
     print(metropolis.sample())
 
-
+    metropolis_multiple = MetroPolis(loglikelihood, shape=(10,))
+    print(metropolis_multiple.sample())
 
 if __name__ == "__main__":
-    metropolis_test()
+    test_metropolis()
